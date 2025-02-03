c0c6fa30617c2e807368b93748a2efb693ad5bd1
shadow6444 <shadowgarden644@gmail.com>
2025-02-03 13:51:23 +0500

    Create userAuth.js

---
+++ server/middleware/userAuth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const jwt_secret_key = "859215";
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decoded = jwt.verify(token, jwt_secret_key);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = verifyToken;
