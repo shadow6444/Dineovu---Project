5b16f5084958cedd1d0b26644dc05378cc5d5b0d
shadow6444 <shadowgarden644@gmail.com>
2025-02-03 13:50:33 +0500

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//Import custom modules
+const { userRouter } = require("./routes/userRoutes.js");
+const { bookingRouter } = require("./routes/bookingRoutes.js");
+const { feedbackRouter } = require("./routes/feedbackRoutes.js");
+const { menuRouter } = require("./routes/menuRoutes.js");
+const { orderRouter } = require("./routes/orderRoutes.js");
+
+// middleware to parse incoming requests
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+// user router use
+app.use("/api/user", userRouter);
+app.use("/api/booking", bookingRouter);
+app.use("/api/feedback", feedbackRouter);
+app.use("/api/menu", menuRouter);
+app.use("/api/order", orderRouter);
+
+// Server setup
+const host = "localhost";
+const port = 3000;
+app.listen(port, host, () =>
+  console.log(`> Server is up and running on http://${host}:${port}`)
+);
