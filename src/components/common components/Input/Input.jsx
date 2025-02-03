0e5e0b06b5baed39a040a2ee981d0f9b76b268b5
shadow6444 <shadowgarden644@gmail.com>
2025-02-03 13:54:53 +0500

    Create Input

---
+++ src/components/common components/Input/Input.jsx
@@ -0,0 +1,40 @@
+import HidePass from "../../../assets/hide-password.svg";
+import ShowPass from "../../../assets/show-password.svg";
+const Input = ({
+  togglePassword,
+  onChange,
+  name,
+  type,
+  image,
+  value,
+  placeholder,
+  password = false,
+  disabled = false,
+}) => {
+  return (
+    <div className="flex items-center px-2 py-2 border border-grayWood rounded-lg w-full">
+      <img src={image} alt="mail image" className="h-5 w-5" />
+      <input
+        value={value}
+        onChange={onChange}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        disabled={disabled}
+        className="border-none focus:ring-0 focus:outline-none pl-3 bg-white placeholder:text-gray-400 w-full"
+      />
+      {password ? (
+        <button type="button" onClick={togglePassword}>
+          <img
+            src={type === "text" ? HidePass : ShowPass}
+            alt="hide password"
+            height={28}
+            width={28}
+          />
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+export default Input;
