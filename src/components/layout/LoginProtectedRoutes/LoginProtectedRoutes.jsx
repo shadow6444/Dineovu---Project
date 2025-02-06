77d01134cdbbf6f570819f80a88fed1701cc53fd
shadow6444 <shadowgarden644@gmail.com>
2025-02-06 16:22:32 +0500

    Update LoginProtectedRoutes.jsx

--- src/components/layout/LoginProtectedRoutes/LoginProtectedRoutes.jsx
+++ src/components/layout/LoginProtectedRoutes/LoginProtectedRoutes.jsx
@@ -1,7 +1,15 @@
+import { jwtDecode } from "jwt-decode";
 import { Navigate, Outlet } from "react-router-dom";
 
 const LoginProtectedRoutes = () => {
-  const isLoggedIn = localStorage.getItem("token");
-  return isLoggedIn ? <Outlet /> : <Navigate to="/auth/login" />;
+  const token = localStorage.getItem("token");
+  const isLoggedIn = token ? jwtDecode(token) : "Not logged in";
+  return isLoggedIn && isLoggedIn.role === "customer" ? (
+    <Outlet />
+  ) : isLoggedIn && isLoggedIn.role === "admin" ? (
+    <Navigate to="/menu" />
+  ) : (
+    <Navigate to="/auth/login" />
+  );
 };
 export default LoginProtectedRoutes;
