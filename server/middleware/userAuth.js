b8a75263328ab3274ac5f403a3a36e87baaa9325
shadow6444 <shadowgarden644@gmail.com>
2025-02-16 20:11:01 +0500

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -2,16 +2,19 @@ const jwt = require("jsonwebtoken");
 
 const jwt_secret_key = "859215";
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  if (!token)
+    return res.status(401).json({ success: false, message: "Unauthorized" });
   try {
     const decoded = jwt.verify(token, jwt_secret_key);
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(403).json({ message: "Invalid or expired token" });
+    res
+      .status(403)
+      .json({ success: false, message: "Invalid or expired token" });
   }
 };
 
-module.exports = verifyToken;
+module.exports = { verifyToken };
